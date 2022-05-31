c8a6e4de611964affb0d815d5767478f4a675853
Your Name <you@example.com>
2022-05-31 12:03:12 +0200

    Add Curriencies model to django project

--- currencyConverter/fetchFromApi/models.py
+++ currencyConverter/fetchFromApi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class currencies(models.Model):
+    fromCurrency = models.CharField(max_length=10)
+    toCurrency = models.CharField(max_length=10)
+    rate = models.FloatField()
