2b509619ed1507ee585107da783d95d8d9290f0f
Your Name <you@example.com>
2022-06-02 17:24:32 +0200

    Update script that posts to DB

--- currencyConverter/scripts/PostToDB.py
+++ currencyConverter/scripts/PostToDB.py
@@ -5,16 +5,13 @@ from currencyConverter.settings import API_KEY
 
 
 def run():
-    Curriencies=["AED","AUD","CAD","EGP","EUR","GBP","INR","PLN","SAR","ZAR"]
-    for from_currency in Curriencies:
-            for to_currency in Curriencies:
-                url = "https://api.apilayer.com/exchangerates_data/convert?to="+to_currency+"&from="+from_currency+"&amount=20"
-                payload = {}
-                headers= {
-                "apikey":API_KEY
-                }
-                response = requests.request("GET", url, headers=headers, data = payload)
-                result = response.json()
-                currencies.objects.create(fromCurrency=from_currency,toCurrency=to_currency, rate=result['info']['rate'])
-
-
+    url ="https://api.apilayer.com/exchangerates_data/latest?&base=EGP"
+    payload = {}
+    headers= {
+    "apikey":API_KEY
+    }
+    response = requests.request("GET", url, headers=headers, data = payload)
+    result = response.json()
+    for currency in result['rates']:
+        currencies.objects.create(fromCurrency='EGP',toCurrency=currency, rate=result['rates'][currency])
+        print(currency,result['rates'][currency])
