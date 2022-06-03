f47d5dc95caa80d857a855bfcfe8748403ce7a2a
Your Name <you@example.com>
2022-06-03 16:59:10 +0200

    Setup currency converter api app

--- currencyConverter/currencyConverter/settings.py
+++ currencyConverter/currencyConverter/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'django_apscheduler',
-    'fetchFromApi'
+    'fetchFromApi',
+    'currencyConverterApi'
 ]
 
 MIDDLEWARE = [
---
+++ currencyConverter/currencyConverterApi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CurrencyconverterapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'currencyConverterApi'
